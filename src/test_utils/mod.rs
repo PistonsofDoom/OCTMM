fa0d5cd07a0f45466cfc1d79273391c8ea63439a
PistonsofDoom <=>
2025-04-15 20:54:37 -0500

    feat: test utilities

---
+++ src/test_utils/mod.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+/// Removes the directory `OCTMM/tmp/`, 
+/// including the contents. Returns
+/// Some(PathBuf) on success, and None
+/// on failure
+pub fn get_test_dir() -> Option<PathBuf> {
+    let mut current_dir = env::current_dir().unwrap();
+    current_dir.push("tmp");
+
+    let _ = fs::remove_dir_all(&current_dir);
+    fs::create_dir(&current_dir).ok()?;
+
+    if current_dir.exists() {
+        Some(current_dir)
+    }
+    else {
+        None
+    }
+}
