cb47b00cf456341902355b70ea82c31df45e5977
PistonsofDoom <=>
2025-05-26 17:49:34 -0500

    feat: basic network functionality outlined, test TODOs added

--- src/runner/dsp/mod.rs
+++ src/runner/dsp/mod.rs
@@ -81,13 +81,79 @@ impl DspModule {
     }
 
     /* Network Management Functions */
-    pub fn net_reserve(&mut self) -> usize {
+    // NOTE: as it is right now, to create complex networks, multiple "temporary networks" need to
+    // be created, which are then combined together in various ways (e.g., summing, mixing, piping)
+    // This is fine, but the previous steps stay in memory. This could potentially be optimized by
+    // implementing a "temporary network" index
+    pub fn net_exists(&mut self, target_net: usize) -> bool {
+        return target_net < self.nets.len();
+    }
+
+    pub fn net_new(&mut self) -> usize {
         self.nets.push(Net::new(0, 2));
         return self.nets.len() - 1;
     }
 
+    pub fn net_from(&mut self, new_network: &Net) -> usize {
+        self.nets.push(new_network.clone());
+        return self.nets.len() - 1;
+    }
+
+    /* Network Combination Functions */
+    pub fn net_pipe(&mut self, target_a: usize, target_b: usize) -> Option<usize> {
+        if !self.net_exists(target_a) || !self.net_exists(target_b) {
+            return None;
+        }
+
+        let net_a = self.nets[target_a].clone();
+        let net_b = self.nets[target_b].clone();
+
+        if !Net::can_pipe(&net_a, &net_b) {
+            return None;
+        }
+
+        let new_network = Net::pipe(net_a, net_b);
+
+        Some(self.net_from(&new_network))
+    }
+
+    pub fn net_product(&mut self, target_a: usize, target_b: usize) -> Option<usize> {
+        if !self.net_exists(target_a) || !self.net_exists(target_b) {
+            return None;
+        }
+
+        let net_a = self.nets[target_a].clone();
+        let net_b = self.nets[target_b].clone();
+
+        if !Net::can_product(&net_a, &net_b) {
+            return None;
+        }
+
+        let new_network = Net::product(net_a, net_b);
+
+        Some(self.net_from(&new_network))
+    }
+
+    pub fn net_sum(&mut self, target_a: usize, target_b: usize) -> Option<usize> {
+        if !self.net_exists(target_a) || !self.net_exists(target_b) {
+            return None;
+        }
+
+        let net_a = self.nets[target_a].clone();
+        let net_b = self.nets[target_b].clone();
+
+        if !Net::can_sum(&net_a, &net_b) {
+            return None;
+        }
+
+        let new_network = Net::sum(net_a, net_b);
+
+        Some(self.net_from(&new_network))
+    }
+
+    /* Network Usage Functions */
     pub fn net_push(&mut self, target_net: usize, node_type: NodeType) -> Option<NodeId> {
-        if target_net >= self.nets.len() {
+        if !self.net_exists(target_net) {
             return None;
         }
 
@@ -95,14 +161,19 @@ impl DspModule {
     }
 
     pub fn net_chain(&mut self, target_net: usize, node_type: NodeType) -> Option<NodeId> {
-        if target_net >= self.nets.len() {
+        if !self.net_exists(target_net) {
             return None;
         }
 
         Some(self.nets[target_net].chain(node_type.as_unit()))
     }
 
-    pub fn net_
+    pub fn net_commit(&mut self, target_net: usize) {
+        if self.net_exists(target_net) {
+            self.nets[target_net].commit();
+        }
+    }
+
 }
 
 impl Module for DspModule {
@@ -115,4 +186,21 @@ impl Module for DspModule {
 mod tests {
     use crate::runner::DspModule;
     use fundsp::hacker32::*;
+
+    /* Network Testing */
+    #[test]
+    pub fn test_net_vector() {
+        // TODO: Test all vector facing functions (e.g., net_exists, net_reserve, net_from
+    }
+
+    #[test]
+    pub fn test_net_combination() {
+        // TODO: Test all network combination functions
+    }
+
+    #[test]
+    pub fn test_net_usage()
+    {
+        // TODO: Test all but net_commit under "Usage Functions"
+    }
 }
