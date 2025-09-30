be9b08804345e3cd6d2f962e35299670b6713c97
PistonsofDoom <=>
2025-09-29 23:11:45 -0500

    refactor: cleaned up net_effect argument handling

--- src/runner/audio/dsp/mod.rs
+++ src/runner/audio/dsp/mod.rs
@@ -622,59 +622,59 @@ impl CommandModule for DspModule {
             "net_effect" => {
                 let arg_type = arg_vec.get(1).expect("net_effect, type not found");
 
-                // TODO: Before merge, replace with while loop & vector for cleanliness
-                let arg1 = arg_vec
-                    .get(2)
-                    .expect("net_effect, arg1 not found")
-                    .parse::<f32>()
-                    .expect("net_effect, arg1 f32 conversion");
-                let arg2 = arg_vec
-                    .get(3)
-                    .expect("net_effect, arg2 not found")
-                    .parse::<f32>()
-                    .expect("net_effect, arg2 f32 conversion");
-                let arg3 = arg_vec
-                    .get(4)
-                    .expect("net_effect, arg3 not found")
-                    .parse::<f32>()
-                    .expect("net_effect, arg3 f32 conversion");
-                let arg4 = arg_vec
-                    .get(5)
-                    .expect("net_effect, arg4 not found")
-                    .parse::<f32>()
-                    .expect("net_effect, arg4 f32 conversion");
-                let arg5 = arg_vec
-                    .get(6)
-                    .expect("net_effect, arg5 not found")
-                    .parse::<f32>()
-                    .expect("net_effect, arg5 f32 conversion");
-                let arg6 = arg_vec
-                    .get(7)
-                    .expect("net_effect, arg6 not found")
-                    .parse::<f32>()
-                    .expect("net_effect, arg6 f32 conversion");
+                // Initialize all arguments as 0
+                let mut args: [f32; 6] = [0.0; 6];
+
+                // Check for up to 6 provided arguments
+                for index in 0..6 {
+                    let arg = arg_vec.get(2 + index);
+
+                    if arg.is_none() {
+                        break;
+                    }
+                    let arg = arg
+                        .expect("Failed to find net_effect argument")
+                        .parse::<f32>()
+                        .expect("net_effect, failed to convert arugment to f32");
+
+                    args[index] = arg;
+                }
 
                 return match *arg_type {
                     "reverb" => self
-                        .net_from(&Net::wrap(Box::new(reverb_stereo(arg1, arg2, arg3))))
+                        .net_from(&Net::wrap(Box::new(reverb_stereo(
+                            args[0], args[1], args[2],
+                        ))))
                         .to_string(), // 3 arg
-                    "delay" => self.net_from(&Net::wrap(Box::new(delay(arg1)))).to_string(), // 1 arg
+                    "delay" => self
+                        .net_from(&Net::wrap(Box::new(delay(args[0]))))
+                        .to_string(), // 1 arg
                     "flanger" => self
-                        .net_from(&Net::wrap(Box::new(flanger(arg1, arg2, arg3, move |t| {
-                            lerp11(arg4, arg5, sin_hz(arg6, t))
-                        }))))
+                        .net_from(&Net::wrap(Box::new(flanger(
+                            args[0],
+                            args[1],
+                            args[2],
+                            move |t| lerp11(args[3], args[4], sin_hz(args[5], t)),
+                        ))))
                         .to_string(), // 6 arg, 4, 5, and 6 are used to tune the flanger closure
                     "pluck" => self
-                        .net_from(&Net::wrap(Box::new(pluck(arg1, arg2, arg3))))
+                        .net_from(&Net::wrap(Box::new(pluck(args[0], args[1], args[2]))))
                         .to_string(), // 3 arg
                     "chorus" => self
-                        .net_from(&Net::wrap(Box::new(chorus(arg4 as u64, arg1, arg2, arg3))))
-                        .to_string(), // 4 arg, arg4 is used for the u64 seed, optionally
+                        .net_from(&Net::wrap(Box::new(chorus(
+                            args[3] as u64,
+                            args[0],
+                            args[1],
+                            args[2],
+                        ))))
+                        .to_string(), // 4 arg, args[3] is used for the u64 seed, optionally
                     "adsr_live" => self
-                        .net_from(&Net::wrap(Box::new(adsr_live(arg1, arg2, arg3, arg4))))
+                        .net_from(&Net::wrap(Box::new(adsr_live(
+                            args[0], args[1], args[2], args[3],
+                        ))))
                         .to_string(), // 4 arg
                     "clip_to" => self
-                        .net_from(&Net::wrap(Box::new(clip_to(arg1, arg2))))
+                        .net_from(&Net::wrap(Box::new(clip_to(args[0], args[1]))))
                         .to_string(), // 2 arg
                     _ => "nil".to_string(),
                 };
